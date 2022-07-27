1bf958eaf835bf7ad844e35d151671da81336f81
samuel-sanp <samuel.junior@sanp.com.br>
2022-07-27 10:37:46 -0300

    comenta código de renderização dos dados pelo contrutor do bloco.

--- app/code/local/Inchoo/Adminblock/Block/Adminhtml/Adminblock.php
+++ app/code/local/Inchoo/Adminblock/Block/Adminhtml/Adminblock.php
@@ -3,14 +3,14 @@
 class Inchoo_Adminblock_Block_Adminhtml_Adminblock extends Mage_Core_Block_Template
 {
 
-    /**
-     * Initialize template
-     *
-     */
-    protected function _construct()
-    {
-        $this->setTemplate('inchoo/adminblock.phtml');
-    }
+    // /**
+    //  * Initialize template
+    //  *
+    //  */
+    // protected function _construct()
+    // {
+    //     $this->setTemplate('inchoo/adminblock.phtml');
+    // }
 
     public function getName()
     {
