8ad8c45ea8a6c17672b1a819da7fa3f1fa4173e8
samuel-sanp <samuel.junior@sanp.com.br>
2022-07-28 10:15:08 -0300

    implementa script par atualização do banco de dados.

--- app/code/local/Magentotutorial/Weblog/sql/weblog_setup/mysql4-install-0.1.0.php
+++ app/code/local/Magentotutorial/Weblog/sql/weblog_setup/mysql4-install-0.1.0.php
@@ -15,3 +15,27 @@ $installer->run("
     INSERT INTO `{$installer->getTable('weblog/blogpost')}` VALUES (1,'My New Title','This is a blog post','2009-07-01 00:00:00','2009-07-02 23:12:30');
 ");
 $installer->endSetup();
+
+// $installer = $this;
+// $installer->startSetup();
+// $table = $installer->getConnection()->newTable($installer->getTable('weblog/blogpost'))
+//     ->addColumn('blogpost_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+//         'unsigned' => true,
+//         'nullable' => false,
+//         'primary' => true,
+//         'identity' => true,
+//         ), 'Blogpost ID')
+//     ->addColumn('title', Varien_Db_Ddl_Table::TYPE_TEXT, null, array(
+//         'nullable' => false,
+//         ), 'Blogpost Title')
+//     ->addColumn('post', Varien_Db_Ddl_Table::TYPE_TEXT, null, array(
+//         'nullable' => true,
+//         ), 'Blogpost Body')
+//     ->addColumn('date', Varien_Db_Ddl_Table::TYPE_DATETIME, null, array(
+//         ), 'Blogpost Date')
+//     ->addColumn('timestamp', Varien_Db_Ddl_Table::TYPE_TIMESTAMP, null, array(
+//         ), 'Timestamp')
+//     ->setComment('Magentotutorial weblog/blogpost entity table');
+// $installer->getConnection()->createTable($table);
+
+// $installer->endSetup();
