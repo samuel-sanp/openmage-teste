5760a82f079811fd44329d5cc67c6e0bc99cb8f1
samuel-sanp <samuel.junior@sanp.com.br>
2022-07-28 08:15:17 -0300

    implementa crud model.

--- app/code/local/Magentotutorial/Weblog/controllers/IndexController.php
+++ app/code/local/Magentotutorial/Weblog/controllers/IndexController.php
@@ -2,6 +2,41 @@
 
 class Magentotutorial_Weblog_IndexController extends Mage_Core_Controller_Front_Action {
     public function testModelAction() {
-        echo 'Setup!';
+        $params = $this->getRequest()->getParams();
+        $blogpost = Mage::getModel('weblog/blogpost');
+        $blogpost->load($params['id']);
+        $data = $blogpost->getData();
+        var_dump($data);
+    }
+
+    public function createNewPostAction() {
+        $blogpost = Mage::getModel('weblog/blogpost');
+        $blogpost->setTitle('Codificar novo Post!');
+        $blogpost->setPost('Este post foi criado a partir de codigo!');
+        $blogpost->save();
+        echo 'post com ID '.$blogpost->getId().' criado';
+    }
+
+    public function editFirstPostAction() {
+        $blogpost = Mage::getModel('weblog/blogpost');
+        $blogpost->load(1);
+        $blogpost->setTitle('O primeiro post!');
+        $blogpost->save();
+        echo 'post com ID '.$blogpost->getId().' editado';
+    }
+
+    public function deleteSecondPostAction() {
+        $blogpost = Mage::getModel('weblog/blogpost');
+        $blogpost->load(2);
+        $blogpost->delete();
+        echo 'post removido';
+    }
+
+    public function showAllBlogPostAction() {
+        $posts = Mage::getModel('weblog/blogpost')->getCollection();
+        foreach($posts as $blogpost) {
+            echo '<h3>'.$blogpost->getTitle().'</h3>';
+            echo nl2br($blogpost->getPost());
+        }
     }
 }
