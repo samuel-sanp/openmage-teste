c98d5496f451e6785e92c562ce00a9bac1ac2f4c
samuel-sanp <samuel.junior@sanp.com.br>
2022-07-29 16:30:58 -0300

    implementa modelo EAV.

--- app/code/local/Magentotutorial/Complexworld/controllers/IndexController.php
+++ app/code/local/Magentotutorial/Complexworld/controllers/IndexController.php
@@ -1,7 +1,40 @@
 <?php
 class Magentotutorial_Complexworld_IndexController extends Mage_Core_Controller_Front_Action {
     public function indexAction() {
+        $weblog2 = Mage::getModel('complexworld/eavblogpost');
+        $weblog2->load(1);
+        
         $this->loadLayout();
         $this->renderLayout();
+    }
+
+    public function populateEntriesAction() {
+        for ($i=0; $i<10; $i++) {
+            $weblog2 = Mage::getModel('complexworld/eavblogpost');
+            $weblog2->setTitle('Test title '.$i);
+            $weblog2->setContent('Test content '.$i);
+            $weblog2->setDate(now());
+            $weblog2->save();
+        }
+
+        echo 'Done';
+    }
+
+    public function showCollectionAction() {
+        $weblog2 = Mage::getModel('complexworld/eavblogpost');
+        $entries = $weblog2->getCollection()
+            ->addAttributeToSelect('title')
+            ->addAttributeToSelect('date')
+            ->addAttributeToSelect('content');
+        $entries->load();
+
+        foreach($entries as $entry)
+        {
+            echo '<h2>' . $entry->getTitle() . '</h2>';
+            echo '<p>Date: ' . $entry->getDate() . '</p>';
+            echo '<p>' . $entry->getContent() . '</p>';
+        }
+
+        echo '</br>Done</br>';
     }
 }
---
+++ app/code/local/Magentotutorial/Complexworld/sql/complexworld_setup/install-0.1.0.php
@@ -0,0 +1,46 @@
+<?php
+
+$installer = $this;
+$installer->startSetup();
+$installer->addEntityType(
+    'complexworld_eavblogpost',
+    array(
+        'entity_model' => 'complexworld/eavblogpost',
+        'table' => 'complexworld/eavblogpost',
+    )
+);
+
+// $installer->createEntityTables(
+//     $this->getTable('complexworld/eavblogpost')
+// );
+
+$this->addAttribute(
+    'complexworld_eavblogpost',
+    'title',
+    array(
+        'type'              => 'varchar',
+        'label'             => 'Title',
+        'input'             => 'text',
+        'class'             => '',
+        'backend'           => '',
+        'frontend'          => '',
+        'source'            => '',
+        'required'          => true,
+        'user_defined'      => true,
+        'default'           => '',
+        'unique'            => false,
+    )
+);
+$this->addAttribute('complexworld_eavblogpost', 'content', array(
+    'type'              => 'text',
+    'label'             => 'Content',
+    'input'             => 'textarea',
+));
+$this->addAttribute('complexworld_eavblogpost', 'date', array(
+    'type'              => 'datetime',
+    'label'             => 'Post Date',
+    'input'             => 'datetime',
+    'required'          => false,
+));
+
+$installer->endSetup();
