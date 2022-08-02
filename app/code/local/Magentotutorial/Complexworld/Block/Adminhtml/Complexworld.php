6af393d8c7bb423cb279b999fa5bebb0475c8dcf
samuel-sanp <samuel.junior@sanp.com.br>
2022-08-02 14:04:38 -0300

    adiciona botoes sem reação.

--- app/code/local/Magentotutorial/Complexworld/Block/Adminhtml/Complexworld.php
+++ app/code/local/Magentotutorial/Complexworld/Block/Adminhtml/Complexworld.php
@@ -1,15 +1,34 @@
 <?php
 
-class Magentotutorial_Complexworld_Block_Adminhtml_Complexworld extends Mage_Core_Block_Template
+class Magentotutorial_Complexworld_Block_Adminhtml_Complexworld extends Mage_Adminhtml_Block_Widget_Container
 {
-
-    protected function _construct()
+    public function getPosts()
     {
-        $this->setTemplate('magentotutorial_complexworld/complexworld.phtml');
+        $weblog = Mage::getModel('complexworld/eavblogpost');
+        $entries = $weblog->getCollection()
+            ->addAttributeToSelect('title')
+            ->addAttributeToSelect('content')
+            ->addAttributeToSelect('date');
+        // $entries->load();
+
+        return $entries;
     }
 
-    public function getName()
+    protected function _prepareLayout()
     {
-        return ["name" => "samuel"];
+        // $this->removeButton('add');
+        $this->_addButton('add_new', array(
+            'label' => $this->__('Add'),
+            'onclick' => "setLocation('{$this->getUrl('*/*/testAction')}')",
+        ));
+        $this->_addButton('edit', array(
+            'label' => $this->__('Edit'),
+            'onclick' => "setLocation('{$this->getUrl('*/*/testAction')}')",
+        ));
+        $this->_addButton('remove', array(
+            'label' => $this->__('Remove'),
+            'onclick' => "setLocation('{$this->getUrl('*/*/testAction')}')",
+        ));
+        return parent::_prepareLayout();
     }
 }
--- app/code/local/Magentotutorial/Complexworld/controllers/Adminhtml/IndexController.php
+++ app/code/local/Magentotutorial/Complexworld/controllers/Adminhtml/IndexController.php
@@ -4,19 +4,6 @@ class Magentotutorial_Complexworld_Adminhtml_IndexController extends Mage_Adminh
 {
     public function indexAction()
     {   
-        // $this->loadLayout()
-        // ->_addContent(
-        //     $this->getLayout()
-        //     ->createBlock('magentotutorial_complexworld/adminhtml_complexworld')
-        //     ->setTemplate('magentotutorial_complexworld/complexworld.phtml')
-        // );
-        // return $this->renderLayout();
-
-        // $this->loadLayout();
-        // $block = $this->getLayout()->createBlock('magentotutorial_complexworld/adminhtml_complexworld');
-        // $this->getLayout()->getBlock('content')->append($block);
-        // return $this->renderLayout();
-
         $this->loadLayout();
         return $this->renderLayout();
     }
