695a6efece4e8249cb00921a48e76fc2c3527925
samuel-sanp <samuel.junior@sanp.com.br>
2022-08-04 15:33:01 -0300

    mostra campos de formulário.

--- app/code/local/Magentotutorial/Complexworld/Block/Adminhtml/Index/Edit/Form.php
+++ app/code/local/Magentotutorial/Complexworld/Block/Adminhtml/Index/Edit/Form.php
@@ -7,6 +7,11 @@
  */
 class Magentotutorial_Complexworld_Block_Adminhtml_Index_Edit_Form extends Mage_Adminhtml_Block_Widget_Form
 {
+    public function getPost()
+    {
+        return Mage::registry('current_post_instance');
+    }
+
     /**
      * Prepare form before rendering HTML
      *
@@ -14,18 +19,52 @@ class Magentotutorial_Complexworld_Block_Adminhtml_Index_Edit_Form extends Mage_
      */
     protected function _prepareForm()
     {
-        echo
-        '<pre>',
-        var_dump($this),
-        '<pre>';
 
+        // echo
+        // '<pre>',
+        // var_dump($this->getPost()->getTitle() ? true : false),
+        // '<pre>';
+
+        $post = $this->getPost();
         $form = new Varien_Data_Form(array(
             'id' => 'edit_form',
             'action' => $this->getData('action'),
             'method' => 'post'
         ));
+
+        $fieldset = $form->addFieldset('base_fieldset', array('legend' => $this->__('General')));
+        $this->_addElementTypes($fieldset);
+
+        $fieldset->addField(
+            'title',
+            'text',
+            array(
+                'name' => 'title',
+                'label' => $this->__('Title'),
+                'title'    => $this->__('Title'),
+                'class' => '',
+                'required' => true,
+                'disabled' => false,
+            )
+        );
+
+        $fieldset->addField(
+            'content',
+            'textarea',
+            array(
+                'name'               => 'content',
+                'label'              => $this->__('Content'),
+                'title'              => $this->__('Content'),
+                'class'              => '',
+                'required'           => true,
+            )
+        );
+
+        $form->addValues($this->getPost()->getData());
+
         $form->setUseContainer(true);
         $this->setForm($form);
+
         return parent::_prepareForm();
     }
 }
